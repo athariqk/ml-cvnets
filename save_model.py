3fec24e7d4437ae52beaa8c57cbe7686ed9efc39
athariqk <alib.athariq@gmail.com>
2025-05-15 15:10:31 +0700

    added save_model.py

---
+++ save_model.py
@@ -0,0 +1,18 @@
+from typing import List, Optional
+from cvnets import get_model
+from options.opts import get_training_arguments
+import torch
+
+
+def main(args: Optional[List[str]] = None, **kwargs):
+    opts = get_training_arguments(args=args)
+    print(opts)
+    
+    model = get_model(opts)
+    
+    torch.save(model, "coco-ssd-mobilevitv2-0.75_structure_2class.pt")
+
+    model.info()
+
+if __name__ == "__main__":
+    main()
